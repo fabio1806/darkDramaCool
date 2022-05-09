38ae20d12057a9faec9389b909e984c635dcb001
fabio1806 <74788284+fabio1806@users.noreply.github.com>
2022-05-09 18:23:38 +0200

    Update darkDramaCool.user.js

--- darkDramaCool.user.js
+++ darkDramaCool.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DramaCool dark
 // @namespace    http://tampermonkey.net/
-// @version      0.0.1
+// @version      0.0.2
 // @description  try to make it a bit darker
 // @author       Fabio1806
 // @match        *://*.dramacool.ee/*
